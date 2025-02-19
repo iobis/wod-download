70d205c3fb840b3da92a825706ba64d99a91d430
Pieter Provoost <pieterprovoost@gmail.com>
2025-02-19 15:08:15 +0100

    aws fixes

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # wod-download
 
-Script to download the WOD from the THREDDS server at <https://www.ncei.noaa.gov/thredds-ocean/catalog/ncei/wod/catalog.html>.
+Scripts to download the WOD from the THREDDS server at <https://www.ncei.noaa.gov/thredds-ocean/catalog/ncei/wod/catalog.html>.
+
+## THREDDS
+
+See <thredds.py>.
+
+## AWS
+
+See <aws.sh>, this creates a copy at <https://iode-wod-backup.s3.amazonaws.com/index.html>.
--- aws.sh
+++ aws.sh
@@ -1,2 +1,5 @@
-aws s3 ls --no-sign-request s3://noaa-wod-pds/ --recursive --human-readable --summarize
-rclone sync --progress --s3-public-url s3://noaa-wod-pds aws:iode-wod-backup
+#!/bin/bash
+
+# aws s3 ls --no-sign-request s3://noaa-wod-pds/ --recursive --human-readable --summarize
+aws s3api put-bucket-lifecycle-configuration --bucket iode-wod-backup --lifecycle-configuration file://lifecycle.json
+rclone sync --ignore-deletes --progress --min-size 1b noaa:noaa-wod-pds s3:iode-wod-backup
